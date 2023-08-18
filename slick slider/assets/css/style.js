a4ae5ab374af3285fa3f9b149b4d8ef0f674f506
pankajyaduvanshi-396 <139863536+pankajyaduvanshi-396@users.noreply.github.com>
2023-08-18 10:44:12 +0530

    Add files via upload

---
+++ slick slider/assets/css/style.js
@@ -0,0 +1,11 @@
+$('.slick').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: false,
+    Infinity: true,
+    autoplaySpeed: 1000,
+    dots: true,
+    arrows: true,
+    prevArrow: '.prev',
+    nextArrow: '.next',
+});
